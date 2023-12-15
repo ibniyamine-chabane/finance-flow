5e657d1bcae4c0a0587bd30ccaa49c7c423019e1
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-12-15 17:01:10 +0100

    adding condition for front end and back end on the url on the router

--- SERVERFINANCE/index.php
+++ SERVERFINANCE/index.php
@@ -10,10 +10,26 @@ try {
         throw new Exception("La page n'existe pas");
     } else {
         $url = explode("/",filter_var($_GET['page'], FILTER_SANITIZE_URL)); // filter that add a security on the url
-        // var_dump($url);
+        var_dump($url);
         // echo "La page demandé est : ".$_GET['page'];
-        if(empty($url[0]) || empty($url[1])) throw new Exception("La page n'existe pas");
-        
+        if(empty($url[0]) || empty($url[1])) throw new Exception("La page n'existe pas"); // error if 
+        switch($url[0]) {
+            case "front" :
+                switch ($url[1]) {
+                    case 'profil': echo "données Json du profil de l'utilisateur connectée";
+                    break;
+                    case 'homepage': echo "données Json des donnés de l'utilisateur transaction et de son port monnaie";
+                    break;
+                    case 'transaction': echo "données Json des de l'utilisateur connectée";
+                    break;
+                    default : throw new Exception("Error Processing Request");     
+                }
+            break;
+            case "back" : echo "la page back end demandée ";
+            break;
+            default : throw new Exception("ERREUR 404"); // in cas were front and back is not written
+            
+        }
     }
 } catch (Exception $e){ // error gestion 
     $msg = $e->getmessage();
